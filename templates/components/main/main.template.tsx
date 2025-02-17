e89497371cbf8752863bd29c52e5aadf94da30fa
Jordan Davidson <jordandavidson1990@hotmail.com>
2025-02-17 22:07:36 +0000

    adds main

--- templates/components/main/main.template.tsx
+++ templates/components/main/main.template.tsx
@@ -1,3 +1,9 @@
+import styles from "../../../styles/Home.module.css";
+
 export const MainTemplate: React.FC = () => {
-  return <div className="main">children</div>;
+  return (
+    <div className={styles.main}>
+      <h5 className={styles.title}>Main page</h5>
+    </div>
+  );
 };
