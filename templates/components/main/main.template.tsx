3910d063f1886c75a135232a363352abc731e014
Jordan Davidson <jordandavidson1990@hotmail.com>
2025-03-28 12:46:23 +0000

    redirect user

--- templates/components/main/main.template.tsx
+++ templates/components/main/main.template.tsx
@@ -1,9 +1,15 @@
+import { log } from "console";
 import styles from "../../../styles/Home.module.css";
+import { GetServerSideProps } from "next";
+import { useEffect } from "react";
 
 export const MainTemplate: React.FC = () => {
+  useEffect(() => {
+    window.location.assign("http://www.theknot.com/jordanandginainlove");
+  }, []);
   return (
     <div className={styles.main}>
-      <h5 className={styles.title}>Main page</h5>
+      <h5 className={styles.title}>Redirecting...</h5>
     </div>
   );
 };
